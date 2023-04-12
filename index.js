2199e8b196910e4638467eed601455d5140c87dd
Kevin <klums24@gmail.com>
2023-04-11 19:15:41 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,74 @@
 // Code your solution in this file!
+
+const returnFirstTwoDrivers = function(arrayOfDrivers){
+    const firstTwo = [...arrayOfDrivers.slice(0,2)];
+    return firstTwo;
+}
+
+//test output for returnFirstTwoDrivers
+//console.log(returnFirstTwoDrivers(["Antonia", "Nuru", "Amari", "Mo"]));
+
+
+const returnLastTwoDrivers = function(arrayOfDrivers){
+    const lastTwo = [...arrayOfDrivers.slice(arrayOfDrivers.length -2)]
+    return lastTwo;
+}
+
+
+
+
+
+//test output for returnLastTwoDrivers
+console.log(returnLastTwoDrivers(["Antonia", "Nuru", "Amari", "Mo"]));
+
+//selectingDrivers
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+//createFareMultiplier
+function createFareMultiplier(multiplier){
+    const finalFare = function(initialFare){
+        return multiplier * initialFare;
+    }
+    return finalFare;
+}
+//console.log(createFareMultiplier(4)(10));
+
+//fareDoubler()
+const fareDoubler = function(initialFare){
+    return createFareMultiplier(2)(initialFare);
+};
+
+//console.log(fareDoubler(10));
+
+//fareTriple()
+const fareTripler = function(initialFare){
+    return createFareMultiplier(3)(initialFare);
+}
+
+//console.log(fareTripler(10));
+
+//selectDifferentDrivers()
+function selectDifferentDrivers(drivers, driverFunction){
+    return driverFunction(drivers);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
